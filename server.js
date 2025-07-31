c84e1748cd2b9ac72836f49c4500bd694f3a25ba
mokshikagigabyte <mokshika7777@gmail.com>
2025-07-31 23:09:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.json());
 connectDB();
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/products', require('./routes/products'));
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/products',require('./routes/products'));
 app.use('/api/cart',cartRoutes);
-app.use('/api/wishlist', require('./routes/wishlist'));
-app.use('/api/payment', require('./routes/payment'));
+app.use('/api/wishlist',require('./routes/wishlist'));
+app.use('/api/payment',require('./routes/payment'));
 
 // Search endpoint
 app.get('/api/search', async (req, res) => {
