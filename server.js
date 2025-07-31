ce8409226deb2f2b82d9965e40bd55f99a3c82bb
mokshikagigabyte <mokshika7777@gmail.com>
2025-07-31 22:13:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/products', require('./routes/products'));
-app.use('/api/cart', require('./routes/cart'));
+app.use('/api/cart', cartRoutes);
 app.use('/api/wishlist', require('./routes/wishlist'));
 app.use('/api/payment', require('./routes/payment'));
 
