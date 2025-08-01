e9d86cb75d4d588860e5c59e95955d40be48450a
mokshikagigabyte <mokshika7777@gmail.com>
2025-08-01 20:43:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// ~/storage/shared/shopeasy-backend/server.js
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
@@ -7,7 +6,6 @@ const productRoutes = require('./routes/products');
 const cartRoutes = require('./routes/cart');
 const wishlistRoutes = require('./routes/wishlist');
 const paymentRoutes = require('./routes/payment');
-const helpRoutes = require('./routes/help');
 
 const app = express();
 connectDB();
@@ -20,7 +18,6 @@ app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/wishlist', wishlistRoutes);
 app.use('/api/payment', paymentRoutes);
-app.use('/api/help', helpRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
